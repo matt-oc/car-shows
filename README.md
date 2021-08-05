402203332ef9ddd573620e377f817bc86b801de1
Matthew O'Connor <Matthew@mbp>
2021-08-06 00:11:04 +0100

    'add image upload for add_event'

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ favicon from https://favicon.io/favicon-generator/
 custom error pages https://www.codementor.io/@lakshyasri/custom-error-pages-in-flask-xrgye5l5e
 pasword reset https://github.com/berat/flask-forgot-password
 litepicker datepicker from https://litepicker.com/
+upload preview https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,18 @@
 // https://litepicker.com/ for date picker
 const picker = new Litepicker({
-    element: document.getElementById('litepicker'),
+    element: document.getElementById('eventDate'),
     format: 'D-MM-YYYY'
   });
+
+// https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript for preview of uploaded image
+// https://stackoverflow.com/questions/5697605/limit-the-size-of-a-file-upload-html-input-element file size limit
+  var loadFile = function(event) {
+  	var image = document.getElementById('output');
+    var eventImage = document.getElementById('eventImage');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    if(event.target.files[0].size > 15999024){
+       alert("Image must be below 16MB!");
+        image.src = "";
+       eventImage.value=""; 
+    };
+};
