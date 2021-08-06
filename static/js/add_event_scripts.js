26ed48634633385171db7dbe1315dfe8235af39c
Matthew O'Connor <Matthew@mbp>
2021-08-06 10:52:24 +0100

    'lint all files and validate'

--- static/js/add_event_scripts.js
+++ static/js/add_event_scripts.js
@@ -1,5 +1,5 @@
 // https://litepicker.com/ for date picker
 const picker = new Litepicker({
-    element: document.getElementById('eventDate'),
-    format: 'D-MM-YYYY'
-  });
+  element: document.getElementById('eventDate'),
+  format: 'D-MM-YYYY'
+});
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,16 +3,18 @@
   var loadFile = function(event) {
   	var image = document.getElementById('output');
     var eventImage = document.getElementById('eventImage');
+    //ensuring the filesize doesnt exceed 16MB
     image.src = URL.createObjectURL(event.target.files[0]);
     if(event.target.files[0].size > 15999024){
        alert("Image must be below 16MB!");
         image.src = "";
        eventImage.value="";
-    };
+    }
 };
 
 
 // adapted from https://codepen.io/astrit/pen/OJPyqyx
+// Hiding the flash messages after a small pause
 window.setTimeout(function() {
-    $(".alert").slideUp(400)
+    $(".alert").slideUp(400);
 }, 4000);
