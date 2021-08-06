5b54d64e8d9715163020fd52fd2413de161f8c2f
Matthew O'Connor <Matthew@mbp>
2021-08-06 12:12:43 +0100

    'turn off debug'

--- app.py
+++ app.py
@@ -221,4 +221,4 @@ def edit_event(event_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
