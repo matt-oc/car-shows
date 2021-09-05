fb385e47745c221b9005142660e6c9871ed90e6d
Matthew O'Connor <Matthew@mbp>
2021-09-05 23:22:14 +0100

    'change google maps function to center on event'

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,49 +1,82 @@
 function initMap() {
 
-  var myOptions = {
-    zoom: 7,
-    scrollwheel: false,
-    center: new google.maps.LatLng(53.318399480787555, -7.666736316345829),
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: [{
-      featureType: 'landscape.natural',
-      elementType: 'all',
-      stylers: [{
-        color: '#f8f8f8',
-        gamma: 5
-      }]
-    }]
-  };
-  map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-
-
   if (window.location.href.split('/').slice(-2)[0] == "view_event") {
     let lat = document.getElementById('gmap_canvas').getAttribute('data-lat')
     let lng = document.getElementById('gmap_canvas').getAttribute('data-lng')
+
+    var myOptions = {
+      zoom: 7,
+      scrollwheel: false,
+      center: new google.maps.LatLng(lat, lng),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{
+        featureType: 'landscape.natural',
+        elementType: 'all',
+        stylers: [{
+          color: '#f8f8f8',
+          gamma: 5
+        }]
+      }]
+    };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+
     def = new google.maps.Marker({
       map: map,
       position: new google.maps.LatLng(lat, lng)
     });
   } else if (window.location.href.substring(window.location.href.lastIndexOf('/') + 1) == "view_map") {
-    let infowindow = new google.maps.InfoWindow();
-let events = JSON.parse(document.getElementById('gmap_canvas').getAttribute('data-map'))
-for (var i = 0; i < events.length; i++) {
 
-    var marker = new google.maps.Marker({
+    var myOptions = {
+      zoom: 7,
+      scrollwheel: false,
+      center: new google.maps.LatLng(53.318399480787555, -7.666736316345829),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{
+        featureType: 'landscape.natural',
+        elementType: 'all',
+        stylers: [{
+          color: '#f8f8f8',
+          gamma: 5
+        }]
+      }]
+    };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+
+    let infowindow = new google.maps.InfoWindow();
+    let events = JSON.parse(document.getElementById('gmap_canvas').getAttribute('data-map'))
+    for (var i = 0; i < events.length; i++) {
+
+      var marker = new google.maps.Marker({
         position: new google.maps.LatLng(events[i].lat, events[i].lng),
         map: map,
         title: events[i].event_name
-    });
+      });
 
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-            infowindow.setContent(events[i].event_name);
-            infowindow.open(map, marker);
+          infowindow.setContent(events[i].event_name);
+          infowindow.open(map, marker);
         }
-    })(marker, i));
-}
-  }
-  else {
+      })(marker, i));
+    }
+  } else {
+
+    var myOptions = {
+      zoom: 7,
+      scrollwheel: false,
+      center: new google.maps.LatLng(53.318399480787555, -7.666736316345829),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{
+        featureType: 'landscape.natural',
+        elementType: 'all',
+        stylers: [{
+          color: '#f8f8f8',
+          gamma: 5
+        }]
+      }]
+    };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+
     def = new google.maps.Marker({
       map: map,
       position: new google.maps.LatLng(53.318399480787555, -7.666736316345829)
